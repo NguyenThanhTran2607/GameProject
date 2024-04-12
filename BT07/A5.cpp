ba8778051fc4d093c81f140b5b1df51994426a1a
NguyenThanhTran2607 <NguyenThanhTran2607@gmail.com>
2024-04-12 13:36:41 +0700

    BT

---
+++ BT07/A5.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+char* weird_string() { 
+   char c[] = “123345”;
+   return c; 
+}
+int main(){
+	char* res = weird_string();
+	cout << res << endl;
+	return 0;
+}
+//nhan xet ve output : viec truy cap den mot bien cuc bo sau khi ham ket thuc
+se dan den loi khong xac dinh, ket qua khi chay chuong trinh co the se khong 
+doan truoc duoc va co the dan den cac loi khong xac dinh
